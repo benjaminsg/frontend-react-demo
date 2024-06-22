29b6bfe927284982e005901ee974794454445e63
Benji Spetter-Goldstein <benjisg@comcast.net>
2024-06-21 23:48:18 -0700

    Fix gh pages deploy

--- src/components/routes/home/HomePage.tsx
+++ src/components/routes/home/HomePage.tsx
@@ -1,9 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Table } from '../../Table';
 import { Header } from '../../Header';
 import salesData from '../../../../data/stackline_frontend_assessment_data_2021.json';
 import '../../../index.css';
 import {ProductWidget} from "../../ProductWidget";
+
+const url = "https://url.com/salesData"
+
+/**
+ * This is where I pretend to make some API call
+ *
+ * fetch(apiUrl)
+ .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    salesData = response.json();
+  })
+ .then(data => {
+    console.log(data);
+  })
+ .catch(error => {
+    console.error('Error:', error);
+  });
+ */
 
 const sales = salesData[0].sales;
 
